f2b143189c0cc2c96c44646811fe7651a550fc4b
21gunni <gmh11@hi.is>
2018-02-16 18:44:09 +0000

    mock objects - þarf að fylla inní restina

---
+++ foodMock.js
@@ -0,0 +1,32 @@
+const foodNova = [
+    {
+        name: 'Hressó',
+        location: 'Austurstræti 20',
+        dates: 'mán-fös',
+        time: '11:30-14:00'
+    },
+    {
+        name: 'Hamborgarafabrikkan',
+        location: 'Höfðatorg, Kringlan, Akureyri',
+        dates: 'Alla daga',
+        time: '11:30-17:00'
+    },
+    {
+        name: 'Eldsmiðjan',
+        location: 'Bragagata 38A',
+        dates: 'mán, þrið',
+        time: '11:00-17:00'
+    }
+]
+
+const foodSiminn = [
+
+]
+
+const food2f1 = [
+
+]
+
+const vodafone = [
+    
+]
