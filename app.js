a91ef4ae3693864d133b6651f6f81d38d30b989f
21gunni <gmh11@hi.is>
2018-02-16 16:14:11 +0000

    Server kominn upp

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
+});
