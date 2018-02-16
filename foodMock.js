e173bbc2de45f5f9fce4cb68be29ea3032e10770
Vilhjálmur Jónsson <vij7@hi.is>
2018-02-16 20:51:03 +0000

    mock object

--- foodMock.js
+++ foodMock.js
@@ -20,13 +20,66 @@ const foodNova = [
 ]
 
 const foodSiminn = [
-
+    {
+        name: 'Arna Ís og Kaffibat',
+        location: 'Eiðistorgi 15',
+        dates: 'fimt, föst',
+        time: '17:00-21:00'
+    },
+    {
+        name: 'Bombay Bazaar',
+        location: 'Ármúla 23',
+        dates: 'fimt, föst',
+        time: '11:30-14:00'
+    },
+    {
+        name: 'Burro',
+        location: 'Veltusund 1',
+        dates: 'fimt, föst',
+        time: '17:00-23:00'
+    },
 ]
 
 const food2f1 = [
+    {
+        name: 'Kopar',
+        location: 'Geirsgata 3',
+        dates: 'mán-mið',
+        time: '17:00-21:00'
+    },
+    {
+        name: 'Le Kock',
+        location: 'Ármúli 42',
+        dates: 'mán-sun',
+        time: '15:00-17:00'
+    },
+    {
+        name: 'Nat Kitchen',
+        location: 'Laugavegur 85',
+        dates: 'mán-sun',
+        time: '17:00-21:00'
+    },
 
 ]
 
 const vodafone = [
-    
+    {
+        name: '17 sortir',
+        location: 'Grandagarður 19',
+        dates: 'mán-sun',
+        time: '10:00-18:00'
+    },
+    {
+        name: 'Bambus',
+        location: 'Borgartún 16',
+        dates: 'mán-sun',
+        time: '15:00-17:00'
+    },
+    {
+        name: 'Nings',
+        location: 'Hlíðasmára 12',
+        dates: 'mán-sun',
+        time: '15:00-22:00'
+    },
+
 ]
