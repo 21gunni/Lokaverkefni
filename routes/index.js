b5f7f60e040ea0e59fb8e8c5ad9875883d88a86f
21gunni <gmh11@hi.is>
2018-04-13 10:37:18 +0000

    færði allt js inní main

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var random = require('../public/javascripts/random');
+var random = require('../public/javascripts/main');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
